2489d2451968911f1833318cadcb5b7510805e05
Daisy Kim <doyeonyyy@gmail.com>
2023-08-30 10:08:51 +0900

    To-do 추가시 섹션별 구분에 적용할 카테고리 만들어줌

--- todolist/Model/Task.swift
+++ todolist/Model/Task.swift
@@ -4,4 +4,18 @@ struct Task {
     var id: Int
     var title: String
     var isCompleted: Bool
+    var category: TaskCategory
+}
+
+enum TaskCategory: String, CaseIterable, Codable {
+    case work
+    case life
+    case shopping
+    
+    func toIndex() -> Int {
+        Self.allCases.firstIndex(of: self) ?? 0
+    }
+    static func category(from index: Int) -> Self? {
+        Self.allCases.indices ~= index ? Self.allCases[index] : nil
+    }
 }
